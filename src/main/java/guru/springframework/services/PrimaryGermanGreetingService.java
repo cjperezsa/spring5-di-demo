ca85c3188fe3b9a46a331d46e60f082848790fbc
cperez <cperez@malaga.eu>
2019-08-22 13:41:31 +0200

    mover la configuración a un factory bean (clase en config)

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -8,15 +8,19 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 5/24/17.
  */
-@Service
-@Primary
-@Profile("de")
+
 public class PrimaryGermanGreetingService implements GreetingService {
 
-	@Autowired
     private GreetingRepository greetingRepository;
 
-    @Override
+    
+    public PrimaryGermanGreetingService(GreetingRepository greetingRepository) {
+		super();
+		this.greetingRepository = greetingRepository;
+	}
+
+
+	@Override
     public String sayGreeting() {
         return greetingRepository.getGermanGreeting();
     }
